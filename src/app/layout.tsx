99ee12335d12a3059902a19e28b73ba2bee82b34
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-12-19 23:31:00 +0530

    updated layout and added logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://admin.nadeemsiyam.com"),
   title: "Admin Panel",
   description: "An admin panel for managing my portfolio website",
+  icons: {
+    icon: "favicon.ico",
+  },
+  creator: "Nadeem M Siyam",
+  authors: [{ name: "Nadeem M Siyam", url: "https://nadeemsiyam.com" }],
 };
 
 export default function RootLayout({
@@ -13,6 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta>
+          <link rel="icon" href="favicon.ico" />
+        </meta>
+      </Head>
       <body className="antialiased bg-(--accent)">{children}</body>
     </html>
   );
